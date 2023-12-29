d989f54f9c66936d8fced5fc2407ae25b77d8efc
Jakub Turkiewicz <jakub.turkiewicz@beekeeper.io>
2023-12-30 00:06:42 +0100

    removed T&P tests

--- tests/teamandpersonal/header.spec.ts
+++
@@ -1,8 +0,0 @@
-import header from '../../pageobjects/teamandpersonal/header-page';
-
-fixture`Team & Personal: Header`.page`https://teamandpersonal.pl`;
-
-test('Phone number with e-mail address', async () => {
-  await header.checkPhone('+48 660 22 77 22');
-  await header.checkEmail('biuro [at] teamandpersonal.pl');
-});
