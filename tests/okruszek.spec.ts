bdc33b97f370b9e7095d0ceae8460c89f4c2fa79
Jakub Turkiewicz <jj.turkiewicz@gmail.com>
2024-05-04 20:43:50 +0200

    fix: disable redirection for okruszek

--- tests/okruszek.spec.ts
+++ tests/okruszek.spec.ts
@@ -1,6 +1,6 @@
 import page from '../pageobjects/okruszek/home-page';
 
-fixture`Okruszek`.page`http://www.okruszek.org.pl`;
+fixture`Okruszek`.page`http://www.okruszek.org.pl`.disablePageReloads;
 
 test('Click on bread should donate', async () => {
   await page.clickOnBread();
